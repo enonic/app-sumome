0e13cfaced0f3bc07f0215706d24964a3ea770fa
Bobby <bellfalasch@gmail.com>
2016-02-04 14:30:38 +0100

    Better error handling

--- src/main/resources/site/filters/sumome-script.js
+++ src/main/resources/site/filters/sumome-script.js
@@ -8,25 +8,37 @@ var view = resolve('sumome-script.html');
 exports.responseFilter = function(req, res) {
     var siteConfig = libs.portal.getSiteConfig();
 
+    // Get our SumoMe ID from config
     var sumomeId = "";
     if (siteConfig['sumomeId']) {
         sumomeId = siteConfig['sumomeId'];
     }
 
+    // Preper params to send into Thymeleaf
     var params = {
         id: sumomeId
     };
 
+    // Render our html with Thyemleaf
     var metadata = libs.thymeleaf.render(view, params);
 
-    if (!res.pageContributions.headEnd) {
-        res.pageContributions.headEnd = [];
+    // Try to add this to head portion of html code
+    try{
+        if ( Array.isArray(res.pageContributions.headEnd) ) {
+            res.pageContributions.headEnd.push(metadata);
+        } else {
+            var headEnd = [];
+            headEnd.push(res.pageContributions.headEnd);
+            headEnd.push(metadata);
+            res.pageContributions.headEnd = headEnd;
+        }
+    } catch (e) {
+        log.error("Exception: " + e.message);
     }
-    res.pageContributions.headEnd.push(metadata);
-
+/*
     if (req.params.debug === 'true') {
         res.applyFilters = false; // Skip other filters
     }
-
+*/
     return res;
 };
