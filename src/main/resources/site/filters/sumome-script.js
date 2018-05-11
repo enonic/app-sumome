1e19e86fbdebd272b5fcab7418d7d497b66533a4
Bellfalasch <bellfalasch@gmail.com>
2018-05-11 15:15:53 +0200

    Add option to add script to bodyEnd instead

--- src/main/resources/site/filters/sumome-script.js
+++ src/main/resources/site/filters/sumome-script.js
@@ -3,13 +3,15 @@ var libs = {
   thymeleaf: require('/lib/xp/thymeleaf')
 };
 
-var view = resolve('sumome-script.html');
+var viewLegacy = resolve('sumome-script.html');
+var viewModern = resolve('sumome-script-modern.html');
 
 exports.responseFilter = function(req, res) {
   var siteConfig = libs.portal.getSiteConfig();
 
   // Get our SumoMe ID from config
   var sumomeId = siteConfig['sumomeId'] || null;
+  var sumomeType = siteConfig['embed'] || 'legacy';
 
   // Is Enonic in live mode?
   var live = (req.mode == 'live');
@@ -22,12 +24,20 @@ exports.responseFilter = function(req, res) {
         live: live
     };
 
+	 var view;
+	 if (sumomeType === 'legacy') {
+		view = viewLegacy;
+	 } else {
+		view = viewModern;
+	 }
+
     // Render our html with Thyemleaf
     var metadata = libs.thymeleaf.render(view, params);
 
     // Try to add this to head portion of html code
-    try{
-        if ( Array.isArray(res.pageContributions.headEnd) ) {
+    try {
+		if (sumomeType === 'legacy') {
+		  if ( Array.isArray(res.pageContributions.headEnd) ) {
             res.pageContributions.headEnd.push(metadata);
         } else {
             var headEnd = [];
@@ -35,6 +45,16 @@ exports.responseFilter = function(req, res) {
             headEnd.push(metadata);
             res.pageContributions.headEnd = headEnd;
         }
+	  } else {
+		  if ( Array.isArray(res.pageContributions.bodyEnd) ) {
+            res.pageContributions.bodyEnd.push(metadata);
+        } else {
+            var bodyEnd = [];
+            bodyEnd.push(res.pageContributions.bodyEnd);
+            bodyEnd.push(metadata);
+            res.pageContributions.bodyEnd = bodyEnd;
+        }
+	  }
     } catch (e) {
         log.error("Exception: " + e.message);
     }
